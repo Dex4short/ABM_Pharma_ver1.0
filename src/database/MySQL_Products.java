f1c0665e0a3e87bfc832bcbe16dadd8671dc5a8f
Trystan211 <fcpfelyn@gmail.com>
2024-12-06 21:56:32 +0800

    Add files via upload

---
+++ src/database/MySQL_Products.java
@@ -0,0 +1,14 @@
+package database;
+
+import oop.Product;
+import oop.enums.ProductCondition;
+
+public class MySQL_Products {
+	
+	public static void insert(Product product, ProductCondition condition) {
+		
+	}
+	public static Product[] select(String category, String word) {
+		return null;
+	}
+}
