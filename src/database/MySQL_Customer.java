d2f3bcbcbeec8f166e1b3cbd282ac75809380167
defaulths <zeeeeee0153@gmail.com>
2024-12-14 01:53:20 +0800

    Add files via upload

--- src/database/MySQL_Customer.java
+++ src/database/MySQL_Customer.java
@@ -1,5 +1,53 @@
 package database;
 
+import oop.Cart;
+import oop.Customer;
+
 public class MySQL_Customer {
+	public static String 
+	table_name = "customer",
+	table_columns[] = {"cust_id", "customer_name", "address"};
+	
+	public static Customer insertCustomer(Customer customer, int counter_no) {
+		customer.setCustomerId(MySQL.nextUID("cust_id", "customer"));
+		MySQL.insert(
+			table_name,
+			table_columns,
+			new Object[] {
+				customer.getCustomerId(),
+				customer.getCustomerName(),
+				customer.getAddress()
+			}
+		);
+		return customer;
+	}
+	public static Customer selectCustomer(int car, int counter_no) {
+		Object result[][] = MySQL.select(
+				table_columns,
+				table_name,
+				"where cart_no=" + cart_no
+		);
+		Cart cart = new Cart(
+				(int)result[i][0],	
+				(int)result[i][1],
+				(int)result[i][2]
+			);
+		return cart;
+		}
+
+	public static void deleteCart(int cart_no) {
+		MySQL.delete(table_name, "where cart_no=" + cart_no);
+	}
+	public static void updateCart(int cart_no, Cart cart, int counter_no) {
+		MySQL.update(
+				table_name,
+				table_columns,
+				new Object[] {
+						cart.getCartNo(),
+						counter_no,
+						cart.getOrders()
+				},
+				"where cart_no=" + cart_no);
+	}
 
 }
