f04d5ad7b14f58ef0db54b507711aa7fcf20bff2
Dexter Pacaña <dextepacanya@gmail.com>
2024-12-18 01:00:32 +0800

    Delete src/system directory

--- src/system/ui/tables/TableDisposals.java
+++
@@ -1,9 +0,0 @@
-package system.ui.tables;
-
-public class TableDisposals extends TableProducts{
-	private static final long serialVersionUID = 4737286913516237032L;
-	
-	public TableDisposals() {
-		
-	}
-}
