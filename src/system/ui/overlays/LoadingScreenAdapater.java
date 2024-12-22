9c68b2920612a7212f50c468678d3850640ae336
Dexter Pacaña <dextepacanya@gmail.com>
2024-12-22 22:46:44 +0800

    Delete src/system/ui directory

--- src/system/ui/overlays/LoadingScreenAdapater.java
+++
@@ -1,8 +0,0 @@
-package system.ui.overlays;
-
-public class LoadingScreenAdapater extends LoadingScreen{
-	private static final long serialVersionUID = -3527138550717290830L;
-
-	@Override
-	public void onLoading(String load_name) {}
-}
